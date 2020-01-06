c8fd3011100bbb2a785b8562ed31e36cf1e51409
sjmacneil <hello@sjmacneil.com>
2020-01-06 12:21:29 -0600

    Update matrix lighting modes enabled. Removes magic toggle for lalt & gui Updates OLED to display lctl & gui status

--- keyboards/crkbd/keymaps/sjmac/rules.mk
+++ keyboards/crkbd/keymaps/sjmac/rules.mk
@@ -14,7 +14,7 @@ SRC +=  ./keymaps/sjmac/glcdfont.c \
         ./lib/logo_reader.c \
         ./lib/mode_icon_reader.c \
         ./lib/timelogger.c \
-#         ./lib/rgb_state_reader.c \
+		#./lib/rgb_state_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/keylogger.c \
 
