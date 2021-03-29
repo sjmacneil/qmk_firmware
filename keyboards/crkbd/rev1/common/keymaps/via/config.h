fb67563ba7694863366c57d03d1ba33571977995
sjmacneil <hello@sjmacneil.com>
2021-03-29 13:09:19 -0500

    Modify crkbd via layout

--- keyboards/crkbd/rev1/common/keymaps/via/config.h
+++ keyboards/crkbd/rev1/common/keymaps/via/config.h
@@ -28,9 +28,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
+#define SPLIT_USB_DETECT
 
 #define USE_SERIAL_PD2
 
@@ -44,3 +45,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 17
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
